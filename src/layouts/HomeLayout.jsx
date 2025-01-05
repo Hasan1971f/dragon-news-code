376c14e703c89e674fc30779b07320907d230abc
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2025-01-05 16:09:41 +0600

    added a Integrate Font, Design navigation and Set up main Layout

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,18 +1,27 @@
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
 
 
 const HomeLayout = () => {
     return (
-        <div>
+        <div className="font-poppins" >
          <header>
             <Header></Header>
             <section className=" w-11/12 mx-auto">
                 <LatestNews></LatestNews>
             </section>
+           
          </header>
-         <nav></nav>
-         <main></main>
+         <nav className="w-11/12 mx-auto py-2">
+         <Navbar></Navbar>
+         </nav>
+         <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+         <aside className="left col-span-3">LeftNavbar</aside>
+         <section className="col-span-6">main content</section>
+         <aside className="col-span-3">Right Navbar</aside>
+
+         </main>
         </div>
     );
 };
